622bc149c619b7264b45307eea073fd717fbb122
Dhruv kaith <kaithdhruv@gmail.com>
2022-03-08 22:54:34 +0530

    First section complete

---
+++ JS/first.js
@@ -0,0 +1,29 @@
+/* let js = "fun";
+let firstName = "jonas";
+
+console.log(firstName);  */
+
+/* const now = 2022;
+const ageJonas = now - 1999;
+const ageSarah = now - 2009;
+console.log(ageJonas);
+console.log(ageSarah);
+
+console.log(ageJonas * 2, ageSarah / 10, 2 ** 3);
+
+console.log(typeof ageJonas); */
+
+//alert("Coding Challenge #1");
+/* let markMass = 78;
+let markHeight = 1.69;
+let johnMass = 92;
+let johnHeight = 1.95;
+
+markBMI = markMass / markHeight ** 2;
+johnBMI = johnMass / johnHeight ** 2;
+const markHigherBMI = markBMI > johnBMI;
+console.log(markBMI, johnBMI, markHigherBMI); */
+/* 
+let age = 19;
+
+console.log(`I like to drink ${age >= 18 ? 'wine🍷' : 'water💦'}`); */
---
+++ JS/string.js
@@ -0,0 +1,33 @@
+/* const firstName = "Jonas";
+const now = 2022;
+const birthYear = now - 1999;
+const job = "teacher"; */
+
+/* const out = "i'm " + firstName + " and i'm " + birthYear + " years old. I'm a " + job + "."; */
+/* 
+const out = `i'm ${firstName} and i'm ${birthYear} years old. I'm a ${job}.`;
+console.log(out);
+
+console.log(`This 
+is a 
+multi line string`); */
+/* 
+let age = 17;
+
+if (age >= 18) {
+    console.log("You can drive a car 🚗");
+} else {
+    let wait = 18 - age;
+    console.log(`You must wait ${wait} years to drive a car 🚗`);
+} */
+
+/* let age = `18`;
+
+if (age == 18) console.log("You are an adult");
+
+if (age === 18) console.log("You are an adult"); */
+/* 
+let a = prompt("What is your age?");
+console.log(a);
+console.log(typeof a);
+ */

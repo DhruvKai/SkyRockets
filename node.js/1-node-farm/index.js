ce047f5e795ce409e1bc154a748bb4e5d4077da3
Dhruv kaith <kaithdhruv@gmail.com>
2022-03-27 12:14:19 +0530

    routing

--- node.js/1-node-farm/index.js
+++ node.js/1-node-farm/index.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
+const http = require('http');
+const url = require('url');
 
+///////////////////////////////////////////
+// files ///////////////////////////
 //Blocking syncounus way
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 // console.log(textIn);
@@ -9,17 +13,40 @@ const fs = require('fs');
 
 //Non blocking Async way
 
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-    console.log(data2);
-    fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
-      console.log(data3);
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     console.log(data2);
+//     fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//       console.log(data3);
 
-      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-        console.log(`Your file has been written 🎉`);
-      });
-    });
-  });
-});
-console.log('will read file');
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//         console.log(`Your file has been written 🎉`);
+//       });
+//     });
+//   });
+// });
+// console.log('will read file');
 //call back hell
+
+/////////////////////////////////
+//server
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === '/' || pathName === '/overview') {
+    res.end('This is overview');
+  } else if (pathName === '/product') {
+    res.end('This is the Product');
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world',
+    });
+    res.end('<h1>page not found! </h1>');
+  }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to request on port 8000');
+});

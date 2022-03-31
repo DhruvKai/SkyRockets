731be1e764f01d24fd9d4db62e07784dff90effa
Dhruv kaith <kaithdhruv@gmail.com>
2022-03-31 11:04:14 +0530

    All crud operations

--- node.js/Express/app.js
+++ node.js/Express/app.js
@@ -1,19 +1,10 @@
+const { fail } = require('assert');
 const express = require('express');
 const fs = require('fs');
 
 const app = express();
 
 app.use(express.json()); //middleware
-
-// app.get('/', (req, res) => {
-//   res
-//     .status(200)
-//     .json({ message: 'Hello from the server side!', app: 'Natours' });
-// });
-
-// app.post('/', (req, res) => {
-//   res.send(`You can post to this end point....`);
-// });
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -25,6 +16,35 @@ app.get('/api/v1/tours', (req, res) => {
     results: tours.length,
     data: {
       tours,
+    },
+  });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  //id is a variable defined we have to pass value of id in url, If we don't speicfy it, there will be an error. If it is given like :id/y? y is optional because of ?
+  console.log(req.params); //readong the parameter from url
+  const id = req.params.id * 1; //converting id to number
+  const tour = tours.find(el => el.id === id);
+
+  // if (id > tours.length) {
+  //   //checking if the tour no is not valid
+  //   return res.status(404).json({
+  //     status: 'fail',
+  //     message: 'invalid id',
+  //   });
+  // }
+  //other way of checking if the tour no is not valid
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid id',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
     },
   });
 });
@@ -48,6 +68,36 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    //checking if the tour no is not valid
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid id',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<updated tour>', //sample place holder for updated response
+    },
+  });
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    //checking if the tour no is not valid
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid id',
+    });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
 });
 
 const port = 3000;

54c85c1b78de73824b1f94e07dafabae6b138f05
Dhruv kaith <kaithdhruv@gmail.com>
2022-04-02 14:58:33 +0530

    hosted DB connceted

--- node.js/Express/app.js
+++ node.js/Express/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware👋');
+  // console.log('Hello from the middleware👋');
   next();
 });
 
--- node.js/Express/server.js
+++ node.js/Express/server.js
@@ -1,8 +1,23 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
-
 // console.log(process.env);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    userNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB connection successful');
+  });
 
 //Start server
 const port = process.env.PORT || 3000;

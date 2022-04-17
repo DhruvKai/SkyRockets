4f1d1ebc46d8447ae15cf60f634eb292790f7edf
Dhruv kaith <kaithdhruv@gmail.com>
2022-04-17 11:57:30 +0530

    protected routes better

--- node.js/Express/routes/userRoutes.js
+++ node.js/Express/routes/userRoutes.js
@@ -12,20 +12,11 @@ router.patch('/resetPassword/:token', authController.resetPassword);
 
 router.use(authController.protect);
 
-router.get(
-  '/me',
-  authController.protect,
-  userController.getMe,
-  userController.getUser
-);
+router.get('/me', userController.getMe, userController.getUser);
 
-router.patch(
-  '/updateMyPassword',
-  authController.protect,
-  authController.updatePassword
-);
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
+router.patch('/updateMyPassword', authController.updatePassword);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
 
 router
   .route('/')
